e3570afcb26416c28ae26c20bb580e258f10b35b
Galina <123162299+gali-pan@users.noreply.github.com>
2023-06-27 13:55:32 +0300

    Create Bot.js

---
+++ Bot.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Bing Bot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Script for search in BING
+// @author       Galina Pankratova
+// @match        https://www.bing.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bing.com
+// @grant        none
+// ==/UserScript==
+
+let links = document.links
+let search_button = document.getElementsByName("search")[0];
+let keywords = ["Продать автомобиль", "купить машину", "Срочно продать машину"];
+let keyword = keywords[getRandom(0, keywords.length)];
+
+if (search_button != undefined) {
+  document.getElementsByName("q")[0].value = keyword;
+  search_button.click();
+} else {
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].href.includes("auto.ru")) {
+      let link = links[i];
+      console.log("Нашел строку " + links[i]);
+      link.click();
+      break;
+    }
+  }
+}
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
