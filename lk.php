8110e2f3ddc7f15c032f28ab3eb09614bf43f063
Galina <123162299+gali-pan@users.noreply.github.com>
2023-07-11 19:51:07 +0300

    Create lk.php

---
+++ lk.php
@@ -0,0 +1,5 @@
+<?php 
+echo '<pre>'; print_r($_SESSION["id"]); echo '</pre>';
+echo '<pre>'; print_r($_SESSION["name"]); echo '</pre>';
+echo '<pre>'; print_r($_SESSION["surname"]); echo '</pre>';
+echo '<pre>'; print_r($_SESSION["email"]); echo '</pre>';
